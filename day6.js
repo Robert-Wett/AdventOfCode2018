afdd41a9ad13344c527ee3820c77168406a6415e
Robert-Wett <rdwettlaufer@gmail.com>
2018-12-12 20:58:37 -0800

    Move input to separate file for day 6

--- day6.js
+++ day6.js
@@ -1,64 +1,4 @@
-let realInput =
-`194, 200
-299, 244
-269, 329
-292, 55
-211, 63
-123, 311
-212, 90
-292, 169
-359, 177
-354, 95
-101, 47
-95, 79
-95, 287
-294, 126
-81, 267
-330, 78
-202, 165
-225, 178
-266, 272
-351, 326
-180, 62
-102, 178
-151, 101
-343, 145
-205, 312
-74, 193
-221, 56
-89, 89
-242, 172
-59, 138
-83, 179
-223, 88
-297, 234
-147, 351
-226, 320
-358, 338
-321, 172
-54, 122
-263, 165
-126, 341
-64, 132
-264, 306
-72, 202
-98, 49
-238, 67
-310, 303
-277, 281
-222, 318
-357, 169
-123, 225`;
-
-let input =
-`1, 1
-1, 6
-8, 3
-3, 4
-5, 5
-8, 9`;
-
-const mDist = (p1, p2) => Math.abs(p2.x - p1.x) + Math.abs(p2.y - p1.y)
+let input = require('fs').readFileSync('./input/day6.txt', { encoding: 'utf8' })
 
 class Point {
 	constructor(x, y, on=false) {
@@ -70,7 +10,7 @@ class Point {
 	}
 
 	distFrom(point) {
-		return mDist(this, point);
+		return Math.abs(point.x - this.x) + Math.abs(point.y - this.y)
 	}
 }
 
@@ -298,7 +238,7 @@ const partTwo = (graph) => {
 	return region.length;
 }
 
-let graph = buildGraph(realInput);
+let graph = buildGraph(input);
 // Part 1
 console.log(getBiggestFiniteArea(graph));
 // Part 2
