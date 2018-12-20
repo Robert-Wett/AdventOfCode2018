b905e5d0443c5bd0beeb360f60d818478e57dc4c
Rob Wettlaufer <robert.wettlaufer@synacor.com>
2018-12-20 11:35:13 -0800

    Day 8-II

--- day8.js
+++ day8.js
@@ -6,34 +6,89 @@ let input = require('fs')
 	.map(Number);
 
 class Node {
-	constructor(numChildren, meta = []) {
+	constructor(numChildren, meta = [], isParent = false, id) {
 		this.numChildren = numChildren;
 		this.meta = meta;
+		this.isParent = isParent;
+		this.id = id;
+		this.children = [];
 	}
+
 	sumMeta() {
 		return this.meta.reduce((acc, meta) => {
 			return acc + meta;
 		}, 0);
 	}
+
+	getNodeValue(sum = 0) {
+		if (this.numChildren) {
+			this.meta.map(m => m - 1).forEach(m => {
+				if (m >= 0 && this.children[m]) {
+					sum += this.children[m].getNodeValue();
+				}
+			});
+
+			return sum;
+		} else {
+			return this.sumMeta();
+		}
+	}
 }
 
 // Part 1
 {
-	const nodes = [];
-	const partOne = input => {
+	const buildNodes = (input, nodes = [], first = false) => {
 		let [numChildren, numMeta] = [input.shift(), input.shift()];
 
 		for (let i = numChildren; i > 0; i--) {
-			partOne(input);
+			buildNodes(input, nodes);
 		}
 
 		let meta = input.splice(0, numMeta);
-		nodes.push(new Node(numChildren, meta));
+		nodes.push(new Node(numChildren, meta, first));
+		return nodes;
 	};
-	partOne(input);
+
+	const nodes = buildNodes(input.slice(), [], true);
 	console.log(
 		nodes.reduce((acc, node) => {
 			return acc + node.sumMeta();
 		}, 0)
 	);
+}
+
+// Part 2
+{
+	function idGen() {
+		let id = 0;
+		return () => id++;
+	}
+
+	const getId = new idGen();
+
+	const buildNodesOnGraph = (input, graph, first = false, parent) => {
+		let [numChildren, numMeta] = [input.shift(), input.shift()];
+		const newNode = new Node(numChildren, [], first, getId());
+		graph[newNode.id] = newNode;
+
+		if (parent) {
+			parent.children.push(newNode);
+		}
+
+		for (let i = numChildren; i > 0; i--) {
+			buildNodesOnGraph(input, graph, false, newNode);
+		}
+
+		let meta = input.splice(0, numMeta);
+		graph[newNode.id].meta = meta;
+	};
+
+	const graph = {};
+	buildNodesOnGraph(input.slice(), graph, true);
+
+	const parent = Object.keys(graph)
+		.map(k => graph[k].isParent && graph[k])
+		.filter(Boolean)[0];
+
+	console.log(parent.getNodeValue(0));
 }
